6d988d2d42cb3327d1011ea048d630050e38bfaa
Jonathan Couldridge <beforan86@gmail.com>
2018-05-02 14:16:45 +0100

    v1.0.0 (#1)
    * added class library project and solution
    
    * added service collection extension methods and sample app to test them
    
    * travis yaml
    
    * package metadata
    
    * readme updates
    
    * fixing badge urls
    
    * version++

---
+++ src/UoN.VersionInformation.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace UoN.VersionInformation.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddVersionInformation(this IServiceCollection services)
+            => services.AddTransient<VersionInformationService>();
+
+        public static void AddVersionInformation(this IServiceCollection services,
+            VersionInformationOptions options)
+            => services.AddTransient(_ => new VersionInformationService(options));
+
+        public static void AddVersionInformation(
+            this IServiceCollection services,
+            Action<VersionInformationOptions> configureOptions)
+        {
+            var options = new VersionInformationOptions();
+            configureOptions.Invoke(options);
+            services.AddVersionInformation(options);
+        }
+    }
+}
